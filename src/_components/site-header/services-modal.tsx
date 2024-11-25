39f8ca38063c2c6ed71a0996aeacce36017642d2
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-11-25 20:02:41 +0100

    feat: implementing services modal

---
+++ src/_components/site-header/services-modal.tsx
@@ -0,0 +1,97 @@
+
+"use client"
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
+import Image from "next/image"
+
+const services = [{
+  title: "Savings",
+  tagline: "We help you save to achieve your goal",
+  img: "piggybank.png"
+}, {
+  title: "Remittance",
+  tagline: "Send money across borders seamlessly.",
+  img: "money.png"
+}, {
+  title: "Virtual cards",
+  tagline: "Experience secure, flexible spending with Wiremi cards",
+  img: "small-virtual-card.png"
+}, {
+  title: "Escrow",
+  tagline: "Hold funds until third party meets your terms",
+  img: "escrow.png"
+}, {
+  title: "Crowdfunding",
+  tagline: "Unlock investment opportunities like never before",
+  img: "coin.png"
+}, {
+  title: "B2B transactions",
+  tagline: "Make business payments fast, secure and seamless",
+  img: "b2b.png"
+},]
+
+
+export default function ServicesModal() {
+  return (
+    <NavigationMenu className="left-12">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="!bg-transparent p-0">Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent className="bg-green-400  md:w-[400px] lg:w-[1000px] xl:w-[1200px]">
+            <ul className="grid gap-3 p-6 lg:grid-cols-[.75fr_1fr] z-[10]">
+              {
+                services.map((item) => (
+                  <ListItem href="/docs" title={item.title} key={item.title} img={item.img}>
+                    {item.tagline}
+                  </ListItem>
+                ))
+              }
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, img, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block bg-[#EEF4FF] select-none space-y-1 rounded-md py-4 px-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="flex gap-2">
+            <div className="p-2 bg-white rounded-full flex items-center justify-center">
+              <Image src={`/${img}`} height={40} width={40} alt="" className="object-cover" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <div className="text-sm text-[#105CE2] font-medium leading-none">{title}</div>
+              <p className="line-clamp-2 text-sm leading-snug text-[#57686F]">
+                {children}
+              </p>
+            </div>
+          </div>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
--- src/_components/site-header/site-header.tsx
+++ src/_components/site-header/site-header.tsx
@@ -4,51 +4,58 @@ import { Button } from "@/components/ui/button";
 import { slugify } from "@/lib/helpers";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
+import ServicesModal from "./services";
+import useHover from "@/hooks/use-hover";
 
 const SiteHeader = () => {
-	const navLinks = [
-		"About us",
-		"Services",
-		"Plans & Pricing",
-		"News",
-		"Contact",
-	];
+  const ref = useRef(null);
+  const hovered = useHover(ref);
+  const navLinks = [
+    "About us",
+    "Services",
+    "Plans & Pricing",
+    "News",
+    "Contact",
+  ];
 
-	return (
-		<header className="bg-[#0149A9] py-4">
-			<div className="flex items-center justify-between gap-3 container">
-				<div className="">
-					<Image
-						src={"/white-logo.png"}
-						height={44}
-						width={140}
-						alt="WireMi Logo"
-					/>
-				</div>
-				<nav className="flex items-center gap-4 text-white font-light">
-					{navLinks.map((link) => (
-						<React.Fragment key={link}>
-							{link !== "Services" ? (
-								<Link href={`/${slugify(link)}`}>{link}</Link>
-							) : (
-								<Button variant={"ghost"} className="!bg-transparent p-0">
-									{link}
-								</Button>
-							)}
-						</React.Fragment>
-					))}
-				</nav>
-				<div className="flex items-center gap-4">
-					<Button className="rounded-full bg-[#105CE2] backdrop-opacity-45 text-white">
-						Sign in
-					</Button>
-					<Button className="rounded-full bg-white text-[#105CE2] hover:text-white hover:bg-[#105CE2] transition-colors duration-300 px-6">
-						Get started
-					</Button>
-				</div>
-			</div>
-		</header>
-	);
+  return (
+    <header className="bg-[#0149A9] py-4">
+      <div className="flex flex-col container">
+        <div className="flex items-center justify-between gap-3">
+          <div className="">
+            <Image
+              src={"/white-logo.png"}
+              height={44}
+              width={140}
+              alt="WireMi Logo"
+            />
+          </div>
+          <nav className="flex items-center gap-4 text-white font-light">
+            {navLinks.map((link) => (
+              <React.Fragment key={link}>
+                {link !== "Services" ? (
+                  <Link href={`/${slugify(link)}`}>{link}</Link>
+                ) : (
+                  <Button variant={"ghost"} className="!bg-transparent p-0" ref={ref}>
+                    {link}
+                  </Button>
+                )}
+              </React.Fragment>
+            ))}
+          </nav>
+          <div className="flex items-center gap-4">
+            <Button className="rounded-full bg-[#105CE2] backdrop-opacity-45 text-white">
+              Sign in
+            </Button>
+            <Button className="rounded-full bg-white text-[#105CE2] hover:text-white hover:bg-[#105CE2] transition-colors duration-300 px-6">
+              Get started
+            </Button>
+          </div>
+        </div>
+        <ServicesModal />
+      </div>
+    </header>
+  );
 };
 export default SiteHeader;
